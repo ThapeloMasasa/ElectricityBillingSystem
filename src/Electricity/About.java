561b5e2481071cbccff5a7371d2e8a66f4e99653
ThapeloMasasa <thapelomasasa5@gmail.com>
2025-02-11 23:52:55 -0500

    additions

--- src/Electricity/About.java
+++ src/Electricity/About.java
@@ -33,19 +33,7 @@ public class About extends JFrame implements ActionListener {
                 "\n remaining amount and also the taxations necessary in the " +
                 "\npurhcases made";
 
-        TextArea t1  = new TextArea(s, 10, 40, Scrollbar.VERTICAL);
-        t1.setEditable(false);
-        t1.setBounds(20, 100, 450, 300);
-        add(t1);
 
-        Font f1 = new Font("RALEWAY", Font.BOLD, 16);
-        t1.setFont(f1);
-
-        Container contentPane = this.getContentPane();
-        t1 = new TextArea();
-
-        JLabel l1 = new JLabel("About Projects");
-        add(l1);
         l1.setBounds(170, 10, 180, 80);
         l1.setForeground(Color.red);
 
