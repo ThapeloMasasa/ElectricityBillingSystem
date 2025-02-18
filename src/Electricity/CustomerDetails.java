729c644ceddd251bc6e6803a06908192923c87b5
ThapeloMasasa <thapelomasasa5@gmail.com>
2025-02-17 23:49:32 -0500

    Customer Details

--- src/Electricity/CustomerDetails.java
+++ src/Electricity/CustomerDetails.java
@@ -2,11 +2,34 @@ package Electricity;
 
 import javax.swing.*;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 
 public class CustomerDetails extends JFrame implements ActionListener {
     JTable t1;
     JButton b1;
     String x[] = {"Customer Name", "Meter Number", "Address", "City", "State", "Email", "Phone"};
+    String y[][] = new String[40][8];
+    int i=0, j=0;
+
+    CustomerDetails(){
+        super("Customer Details");
+        setSize(1200, 650);
+        setLocation(400, 150);
+
+        try{
+            Conn c1 = new Conn();
+            String s1 = "select * from customer";
+            ResultSet  rs = c1.s.executeQuery(s1);
+            while(rs.next()){
+                y[i][j++] = rs.getString("name");
+                y[i][j++] = rs.getString("meter");
+                y[i][j++] = rs.getString("address");
+                y[i][j++] = rs.getString("name");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
 
 }
